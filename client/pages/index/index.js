215e2d92bf311699024db02c54f46864122f6be1
jkby24 <289929553@qq.com>
2018-05-01 15:26:23 +0800

    index page

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -5,14 +5,47 @@ var util = require('../../utils/util.js')
 
 Page({
     data: {
+        info:{
+          phone:"0591-23433299",
+          position:{
+            title:"大梦山游泳馆",
+            latitude: 26.087590,
+            longitude: 119.283940,
+          }
+        },
         movies:[    
             {index:0,url:'./images/1.jpg'} ,      
             {index:1,url:'./images/2.jpg'} ,      
             {index:2,url:'./images/3.jpg'}    
-        ]
+        ],
+        markers: [{
+          // iconPath: "/resources/others.png",
+          // label:{
+          //   content: "大梦山"
+          // },
+          // callout:{
+          //   color:"red"
+          // },
+          id: 0,
+          latitude: 26.087590,
+          longitude: 119.283940,
+        }]
     },
     onLoad: function () {
         this.login();
+    }, 
+    call:function(){
+      wx.makePhoneCall({
+        phoneNumber: this.data.info.phone
+      })
+    },
+    go: function () {
+      wx.openLocation({
+        latitude: this.data.info.position.latitude,
+        longitude: this.data.info.position.longitude,
+        name: this.data.info.position.name,
+        scale: 28
+      }) 
     },
     setUserInfo:function(data){
         wx.setStorage({
